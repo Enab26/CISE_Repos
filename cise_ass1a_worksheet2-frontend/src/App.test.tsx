6af7b75712a1bc2020307416ac5682d25da973b0
Enab26 <anab.ismail2606@gmail.com>
2024-08-10 19:25:49 +1200

    Add .gitattributes file for consistent line endings and file handling for worksheet 2 and 3 (frontend and backend)

--- cise_ass1a_worksheet2-frontend/src/App.test.tsx
+++ cise_ass1a_worksheet2-frontend/src/App.test.tsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
+
 it('renders without crashing', () => {
 const div = document.createElement('div');
 ReactDOM.render(<App />, div);
@@ -11,3 +12,5 @@ describe('Addition', () => {
   expect(2 + 2).toBe(4);
   });
   });
+
+  
